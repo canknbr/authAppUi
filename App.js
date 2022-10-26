555a10dd595a8d815ebf76efb0fb297d03aa7f68
Can Kanbur <cankanbur1@gmail.com>
2022-10-26 16:44:35 +0300

    .

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import SignInScreen from './screens/SignInScreen'
+const Stack = createNativeStackNavigator() 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Home" component={SignInScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
